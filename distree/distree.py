5b1105876ee421e31a63ffaaf8b17341d6022361
Ashley Milsted <ashmilsted@gmail.com>
2018-08-05 17:25:06 -0400

    Raise NotImplementedError

--- distree/distree.py
+++ distree/distree.py
@@ -6,6 +6,7 @@ Created on Wed Jul 25 16:10:00 2018
 
 """
 
+import os
 import filelock
 import pathlib
 
@@ -64,15 +65,15 @@ class Distree():
     Specifies how the taskdata filename is derived from the task_id.
     """
     def get_taskdata_path(self, task_id):
-        assert False, "Example implementation!"
-        return self.data_path + '%s.npy' % task_id
+        raise NotImplementedError
+        return os.path.join(self.data_path, '%s.npy' % task_id)
 
     """
     Specifies the taskdata format. The implementation must save
     the task_id and the parent_id. Otherwise it's up to the implementor.
     """
     def save_task_data(self, taskdata_path, data, task_id, parent_id):
-        assert False, "Example implementation!"
+        raise NotImplementedError
         f = open(taskdata_path, "w")
         f.write('%s\t%s\n' % (task_id, parent_id))
         f.close()
@@ -82,7 +83,7 @@ class Distree():
     and return them separately.
     """
     def load_task_data(self, taskdata_path):
-        assert False, "Example implementation!"
+        raise NotImplementedError
         f = open(taskdata_path, "r")
         task_id, parent_id = f[0].split()
         f.close()
@@ -93,4 +94,4 @@ class Distree():
     This is where the actual content goes! Actually run a task.
     """
     def run_task(self, taskdata_path):
-        pass
+        raise NotImplementedError
--- distree/schedulers.py
+++ distree/schedulers.py
@@ -6,6 +6,8 @@ Created on Wed Jul 25 16:10:00 2018
 
 """
 
+import os
+
 import scipy as sp
 import numpy as np
 
@@ -14,12 +16,14 @@ import uuid
 import sys
 import logging
 
+from shlex import quote
+
 class Sched():
     def get_id(self):
-        return None
+        raise NotImplementedError
 
     def schedule_task(self, taskdata_path):
-        pass
+        raise NotImplementedError
 
 class Sched_Local_SameProc(Sched):
     task_list = []
@@ -55,3 +59,31 @@ class Sched_Local(Sched):
                 + self.scriptargs)
         logging.info('Running: {}'.format(args))
         subprocess.Popen(args)
+
+class Sched_PBS(Sched):
+    def __init__(self, qname, scriptpath, scriptargs='', python_command=sys.executable, res_list='', job_env=''):
+        self.qname = qname
+        self.scriptpath = scriptpath
+        self.scriptargs = scriptargs
+        self.python_command = python_command
+        self.res_list = res_list
+        self.job_env = job_env
+        self.jobname = os.path.basename(scriptpath)
+
+    def get_id(self):
+        return uuid.uuid1() #tasks are assigned UUID's based on the host that schedules them
+
+    def schedule_task(self, taskdata_path):
+        scmd = '%s %s %s %s' % (self.python_command, self.scriptpath, taskdata_path, self.scriptargs)
+
+        qsub_cmd = 'qsub -N %s -q %s -k oe' % (quote(self.jobname), self.qname)
+        if len(self.res_list) > 0:
+            qsub_cmd = qsub_cmd + ' -l %s' % self.res_list
+
+        if len(self.job_env) > 0:
+            qsub_cmd = qsub_cmd + ' -v %s' % self.job_env
+
+        cmd = 'echo %s | %s' % (quote(scmd), qsub_cmd)
+        logging.info('Running: ', cmd)
+        p = subprocess.run(cmd, shell=True)
+        p.check_returncode()
