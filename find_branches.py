7b6148cf950ed45edcb799c8bf20234dac94cd01
Markus Hauru <markus@mhauru.org>
2019-06-28 18:11:36 +0200

    Another dump of work-in-progress on find_branches.py, now including its test too.

--- find_branches.py
+++ find_branches.py
@@ -3,6 +3,8 @@ import logging
 from copy import deepcopy
 import scipy.sparse.linalg as spsla
 import scipy as sp
+import functools as fct
+import operator as opr
 from evoMPS.core_common import eps_l_noop, eps_r_noop
 
 def find_branches(s, pars):
@@ -82,7 +84,47 @@ def find_branches_sparse(s, pars):
     else:
         total_branches = outer_branches
         total_coeffs = outer_coeffs
+    total_coeffs, total_branches = zip(*sorted(
+        zip(total_coeffs, total_branches),
+        key=lambda pair: abs(pair[0]),
+        reverse=True
+    ))
     return total_branches, total_coeffs
+
+def find_blocks_in_basis(A):
+    dim = A.shape[0]
+    corner_sizes = [sp.sum(A[:i,i:]) for i in range(1, dim)]
+    i_min = sp.argmin(corner_sizes)
+    s1 = list(range(i_min+1))
+    s2 = [i for i in range(dim) if i not in s1]
+    while True:
+        candidate_changes = []
+        for i in range(dim):
+            if i in s1:
+                if len(s1) == 1:
+                    cost_change = sp.inf
+                else:
+                    cost_change = (sp.sum(A[s1,i]) - sp.sum(A[i,s2]) -
+                                   A[i,i])
+            else:
+                if len(s2) == 1:
+                    cost_change = sp.inf
+                else:
+                    cost_change = (-sp.sum(A[s1,i]) + sp.sum(A[i,s2])
+                                   - A[i,i])
+            candidate_changes.append(cost_change)
+        i = sp.argmin(candidate_changes)
+        cost_change = candidate_changes[i]
+        if cost_change < 0:
+            if i in s1:
+                s1.remove(i)
+                s2.append(i)
+            else:
+                s2.remove(i)
+                s1.append(i)
+        else:
+            break
+    return s1, s2
 
 def find_two_branches_sparse(s, pars):
     """ Try to find two branches with records on regions L, M, R, where
@@ -96,6 +138,8 @@ def find_two_branches_sparse(s, pars):
     - pars["eps_fidelity"] is the tolerance for the error measure 1 - fidelity
       of the decomposition. If the error is worse than this, the proposed
       branching will be rejected.
+    - pars["eps_M_nonint"] is the tolerance for non-interference on M. It is
+      only used if system_for_records in M and RM.
     - pars["k"] is the number of random product states to use in checking that
       the branch projectors commute with the MPS over M. this is only used if
       system_for_records is 'R' or 'RM', since finding records on 'M' only can
@@ -124,96 +168,65 @@ def find_two_branches_sparse(s, pars):
     # We make a copy of s because we will change its gauge to search for
     # branches: We'll put it into central gauge with the point of canonicality
     # lying within M.
+    s_orig = s
     s = deepcopy(s)
     s.update()
     for i in range(1, i1):
         shift_canpoint_evomps(s, i, 1)
 
-    A_matrices = []
-
-    if 'M' in pars["system_for_records"]:
-        y = sp.eye(dimL)
-        for i in range(i1, i2+1):
-            y = eps_l_noop(y, s.A[i], s.A[i])
-        y = y.conjugate()
-        for i in reversed(range(i1, i2+1)):
-            y = eps_r_noop(y, s.A[i], s.A[i])
-        def C_M_func(X):
-            X = sp.reshape(X, (dimL, dimL))
-            term1 = sp.dot(X, y)
-            term2 = sp.dot(y.conjugate(), X)
-            term3 = X
-            for i in range(i1, i2+1):
-                term3 = eps_l_noop(term3, s.A[i], s.A[i])
-            term3 = term3.conjugate()
-            for i in reversed(range(i1, i2+1)):
-                term3 = eps_r_noop(term3, s.A[i], s.A[i])
-            res = term1 + term2 - 2*term3
-            res = sp.reshape(res, (dimL, dimL))
-            # DEBUG
-            print(term1, term2, term3)  # DEBUG
-            print(res)
-            print()
-            input()
-            # END DEBUG
-            return res
-        C_M = spsla.LinearOperator((dimL**2, dimL**2), C_M_func, dtype=s.typ)
-        # Sample some product states for the purpose of forming A.
+    if 'R' in pars["system_for_records"]:
+        matrices_R = []
         for j in range(pars["k"]):
             M = sp.eye(dimL, dimL)
             for i in range(i1, i2+1):
-                # TODO Should state_i be complex?
-                state_i = sp.random.randn(s.q[i]) 
+                state_i = sp.random.randn(s.q[i])
+                if s.typ == sp.complex_:
+                    state_i = state_i + 1j*sp.random.randn(s.q[i])
                 state_i = state_i/sp.linalg.norm(state_i)
                 A_i = sp.tensordot(s.A[i], state_i, axes=(0, 0))
                 M = sp.dot(M, A_i)
             M = sp.dot(M, M.conjugate().transpose())
-            A_matrices.append(M)
-
-    if 'R' in pars["system_for_records"]:
-        matrices = []
+            matrices_R.append(M)
+    k_factor_R = sp.prod(sp.array(s.q[i1:i2+1], dtype=sp.float_))/pars["k"]
+    matrices_R = [M*k_factor_R for M in matrices_R]
+    if 'M' in pars["system_for_records"]:
+        matrices_M = []
         for j in range(pars["k"]):
-            # TODO Should r be complex?
-            r = sp.random.randn(dimR, dimR) 
+            # TODO Do we want positive definite matrices like below, or should
+            # we only pick product states?
+            r = sp.random.randn(dimR, dimR)
+            if s.typ == sp.complex_:
+                r = r + 1j*sp.random.randn(dimR, dimR)
             r = sp.dot(r, r.conjugate().transpose())
             r = r/sp.linalg.norm(r)
             for i in reversed(range(i1, i2+1)):
                 r = eps_r_noop(r, s.A[i], s.A[i])
-            matrices.append(r)
-        A_matrices = A_matrices + matrices
-        conjugates = [r.conjugate().transpose() for r in matrices]
-        squaresL = [sp.dot(rdg, r) for r, rdg in zip(matrices, conjugates)]
-        squaresR = [sp.dot(r, rdg) for r, rdg in zip(matrices, conjugates)]
-        def C_R_func(X):
-            X = sp.reshape(X, (dimL, dimL))
-            res = sp.zeros_like(X)
-            for i in range(pars["k"]):
-                term1 = sp.dot(squaresL[i], X)
-                term2 = sp.dot(X, squaresR[i])
-                term3 = sp.dot(conjugates[i], sp.dot(X, matrices[i]))
-                res += term1 + term2 - 2*term3
-            res = sp.reshape(res, (dimL, dimL))
-            return res
-        C_R = spsla.LinearOperator((dimL**2, dimL**2), C_R_func, dtype=s.typ)
-
-    if pars["system_for_records"] == "R":
-        C = C_R
-    elif pars["system_for_records"] == "M":
-        C = C_M
-    else:
-        # TODO How should we weigh these two contributions? Should for instance
-        # C_R come with a factor that is the ratio of k to the total number of
-        # matrices?
-        C = C_M + C_R
+            matrices_M.append(r)
+    k_factor_M = dimR/pars["k"]
+    matrices_M = [M*k_factor_M for M in matrices_M]
+    matrices = matrices_R + matrices_M
+    average_norm = sp.average([sp.linalg.norm(M) for M in matrices])
+    matrices = [M/average_norm for M in matrices]
+    conjugates = [r.conjugate().transpose() for r in matrices]
+    squaresL = [sp.dot(rdg, r) for r, rdg in zip(matrices, conjugates)]
+    squaresR = [sp.dot(r, rdg) for r, rdg in zip(matrices, conjugates)]
+    def C_func(X):
+        X = sp.reshape(X, (dimL, dimL))
+        res = sp.zeros_like(X)
+        for i in range(len(matrices)):
+            term1 = sp.dot(squaresL[i], X)
+            term2 = sp.dot(X, squaresR[i])
+            term3 = sp.dot(conjugates[i], sp.dot(X, matrices[i]))
+            term4 = sp.dot(matrices[i], sp.dot(X, conjugates[i]))
+            res += term1 + term2 - term3 - term4
+        res = sp.reshape(res, (dimL, dimL))
+        return res
+    C = spsla.LinearOperator((dimL**2, dimL**2), C_func, dtype=s.typ)
 
     # DEBUG
-    eyeL = sp.reshape(sp.eye(dimL, dtype=s.typ), (dimL**2,))
-    print(sp.linalg.norm(C_M.matvec(eyeL)))
-    print(sp.linalg.norm(C_R.matvec(eyeL)))
-    print(sp.linalg.norm(C.matvec(eyeL)))
-    print(C_M.matvec(eyeL))
-    print(C_R.matvec(eyeL))
-    input()
+    #eyeL = sp.reshape(sp.eye(dimL, dtype=s.typ), (dimL**2,))
+    #print(sp.linalg.norm(C.matvec(eyeL)))
+    #print(C.matvec(eyeL))
     # END DEBUG
 
     # Find the largest magnitude eigenvalue of C. This will be used to scale C
@@ -224,16 +237,38 @@ def find_two_branches_sparse(s, pars):
     # search.
     S, U = spsla.eigsh(C, k=1)
     shift = abs(S[0])
-    scaling_op = spsla.LinearOperator((dimL**2, dimL**2), lambda x: shift*x)
-    C_scaled = C - scaling_op
+    # TODO Make a function that creates this and C_func?
+    def C_scaled_func(X):
+        X = sp.reshape(X, (dimL, dimL))
+        res = -shift*X
+        # TODO This is far from optimal: We could sum up all squares
+        # before-hand, and also use reshapes for term3 and term4. Also, the
+        # last two are the same since all our matrices are hermitian (and in
+        # fact pos. semidef.).
+        for i in range(len(matrices)):
+            term1 = sp.dot(squaresL[i], X)
+            term2 = sp.dot(X, squaresR[i])
+            term3 = sp.dot(conjugates[i], sp.dot(X, matrices[i]))
+            term4 = sp.dot(matrices[i], sp.dot(X, conjugates[i]))
+            res += term1 + term2 - term3 - term4
+        res = sp.reshape(res, (dimL, dimL))
+        return res
+    C_scaled = spsla.LinearOperator((dimL**2, dimL**2), C_scaled_func,
+                                    dtype=s.typ)
+    # DEBUG
+    #C_scaled = C - spsla.LinearOperator((dimL**2, dimL**2), lambda x: shift*x)
+    # END DEBUG
 
     # Find the lowest two eigenpairs of C. Two because we are looking for two
     # branches.
     S, U = spsla.eigsh(C_scaled, k=2)
+    # DEBUG
+    #S, U = spsla.eigsh(C, k=2, which="SM")
+    # END DEBUG
     U_tensor = sp.reshape(U, (dimL, dimL, len(S)))
     S += shift  # Correct for the above shift.
     # DEBUG
-    print("Spectrum of C: {}".format(S))
+    print("Spectrum of C: {} (shift: {})".format(S, shift))
     # END DEBUG
 
     # Find X, a linear combination of U[:,0] and U[:,1] that has a clear
@@ -253,7 +288,7 @@ def find_two_branches_sparse(s, pars):
     U0 = (U0 + U0.conjugate().transpose())/2
     U1 = (U1 + U1.conjugate().transpose())/2
     S, V = sp.linalg.eigh(U0)
-    U1_diag = sp.dot(V.conjugate().transpose(), sp.dot(U1, V)) 
+    U1_diag = sp.dot(V.conjugate().transpose(), sp.dot(U1, V))
     theta = sp.arctan(-U1_diag[-1,-1]/S[-1])
     X = sp.sin(theta)*U0 + sp.cos(theta)*U1
 
@@ -267,86 +302,138 @@ def find_two_branches_sparse(s, pars):
     # of A into two blocks that minimizes this error measure.
     S, V = sp.linalg.eigh(X)
     # DEBUG
-    print(X.shape, theta)  # DEBUG
     print("Spectrum of X: {}".format(S))  # DEBUG
-    print(U0)
-    print(U1)
+    print(sp.linalg.eigh(U0)[0])
+    print(sp.linalg.eigh(U1)[0])
+    print("Whether U0, U1 and X are in the kernel of C:")
+    print(sp.linalg.norm(C.matvec(sp.reshape(U0, (-1,)))))
+    print(sp.linalg.norm(C.matvec(sp.reshape(U1, (-1,)))))
+    print(sp.linalg.norm(C.matvec(sp.reshape(X, (-1,)))))
+    #print(U0)
+    #print(U1)
     # END DEBUG
-    A_matrices_V = [sp.dot(V.conjugate().transpose(), sp.dot(M, V))
-                    for M in A_matrices]
+    matrices_V = [sp.dot(V.conjugate().transpose(), sp.dot(M, V))
+                    for M in matrices]
     # DEBUG
-    #[print(abs(M)) for M in A_matrices]
     #print("="*50)
-    #[print(abs(M)) for M in A_matrices_V]
+    #[print(sp.linalg.norm(M)) for M in matrices]
+    #print("="*50)
+    #[print(sp.linalg.norm(M)) for M in matrices_V]
     #input()
     # END DEBUG
-    A = sum(abs(M)**2 for M in A_matrices_V)
+    A = sum(abs(M)**2 for M in matrices_V)
     print(A)  # DEBUG
-    corner_sizes = [sp.sum(A[:i,i:]) for i in range(1, dimL)]
-    i_min = sp.argmin(corner_sizes)
-    s1 = list(range(i_min+1))
-    s2 = [i for i in range(dimL) if i not in s1]
-    while True:
-        candidate_changes = []
-        for i in range(dimL):
-            if i in s1:
-                if len(s1) == 1:
-                    cost_change = sp.inf
-                else:
-                    cost_change = (sp.sum(A[s1,i]) - sp.sum(A[i,s2]) -
-                                   A[i,i])
-            else:
-                if len(s2) == 1:
-                    cost_change = sp.inf
-                else:
-                    cost_change = (-sp.sum(A[s1,i]) + sp.sum(A[i,s2])
-                                   - A[i,i])
-            candidate_changes.append(cost_change)
-        i = sp.argmin(candidate_changes)
-        cost_change = candidate_changes[i]
-        if cost_change < 0:
-            if i in s1:
-                s1.remove(i)
-                s2.append(i)
-            else:
-                s2.remove(i)
-                s1.append(i)
-        else:
-            break
+    s1, s2 = find_blocks_in_basis(A)
     P1 = sp.dot(V[:,s1], V[:,s1].conjugate().transpose())
     P2 = sp.dot(V[:,s2], V[:,s2].conjugate().transpose())
-    projector_list = [P1, P2]
-    dim_list = [len(s1), len(s2)]
+    projector_list_L = [P1, P2]
     # DEBUG
-    print(sp.linalg.norm(C_M_func(P1)))
-    print(sp.linalg.norm(C_R_func(P1)))
+    print("s1 :", s1)
+    print("s2 :", s2)
+    print(sp.linalg.norm(C_func(P1)))
+    print(sp.linalg.norm(C_func(P2)))
     # END DEBUG
+
+    if "R" in pars["system_for_records"]:
+        # Now that we have the projectors at the boundary of L and M, find the best
+        # ones to match at the boundary of M and R.
+        # TODO Transport P1 and P2 to i2. Take the difference, use that as the
+        # environment to find the best basis. Maybe run the same thing as above to
+        # find the correct partition? Note that it's going to have negative
+        # eigenvalues on one branch and positive on the other, so this should be
+        # easy.
+        P1_env = P1
+        P2_env = P2
+        for i in range(i1, i2+1):
+            P1_env = eps_l_noop(P1_env, s.A[i], s.A[i])
+            P2_env = eps_l_noop(P2_env, s.A[i], s.A[i])
+        env = P1_env - P2_env
+        S, V = sp.linalg.eigh(env)
+        s1 = S > 0
+        s2 = S <= 0
+        print("env spec: ", S)  # DEBUG
+        R1 = sp.dot(V[:,s1], V[:,s1].conjugate().transpose())
+        R2 = sp.dot(V[:,s2], V[:,s2].conjugate().transpose())
+        projector_list_R = [R1, R2]
 
     # Construct the branches.
     branch_list = []
-    for i in range(len(projector_list)):
+    for i in range(len(projector_list_L)):
         branch = deepcopy(s)
-        branch.A[i1-1] = sp.tensordot(branch.A[i1-1], projector_list[i],
+        branch.A[i1-1] = sp.tensordot(branch.A[i1-1], projector_list_L[i],
                                       axes=(2,0))
+        if "R" in pars["system_for_records"]:
+            branch.A[i2] = sp.tensordot(branch.A[i2], projector_list_R[i],
+                                        axes=(2,0))
         branch.update(auto_truncate=True)
         branch_list.append(branch)
+    dim_list_L = [b.D[i1-1] for b in branch_list]
+    dim_list_R = [b.D[i2] for b in branch_list]
 
-    # Compute the coefficients the branches should have.
+    # Compute the coefficients the branches should have. Note that we compute
+    # the fidelity with respect to s_orig, since updating s may have caused
+    # global phase changes.
     # TODO This could be done in a more clever way using canonicality
     # properties of the original MPS/the fact that we know what l and r are and
     # how the branches are related to the original state through virtual
     # projectors. Note that the optimality of this choice already relies on the
     # branches being orthogonal. I'll leave this the way it is for now though,
     # because this is fool-proof and not very costly.
-    coeff_list = [mps_overlap(s, branch) for branch in branch_list]
+    coeff_list = [mps_overlap(s_orig, branch) for branch in branch_list]
 
+    if "M" in pars["system_for_records"]:
+        TM_shp = (dim_list_L[0]*dim_list_L[1], dim_list_R[0]*dim_list_R[1])
+        if 0 in TM_shp:
+            M_nonint = 0
+        else:
+            def TM_func_rmatvec(X):
+                shp = X.shape
+                X = X.conjugate()
+                X = sp.reshape(X, (dim_list_L[0], dim_list_L[1]))
+                for i in range(i1, i2+1):
+                    X = eps_l_noop(X, branch_list[0].A[i], branch_list[1].A[i])
+                X = X.conjugate()
+                X = sp.reshape(X, TM_shp[1])
+                return X
+            def TM_func_matvec(X):
+                shp = X.shape
+                X = sp.reshape(X, (dim_list_R[0], dim_list_R[1]))
+                for i in reversed(range(i1, i2+1)):
+                    X = eps_r_noop(X, branch_list[0].A[i], branch_list[1].A[i])
+                X = sp.reshape(X, TM_shp[0])
+                return X
+            TM = spsla.LinearOperator(TM_shp, matvec=TM_func_matvec,
+                                      rmatvec=TM_func_rmatvec, dtype=s.typ)
+            if 1 in TM_shp or 2 in TM_shp:
+                TM_dense = TM.matmat(sp.eye(TM_shp[1]))
+                M_nonint = sp.linalg.svd(TM_dense)[1][0]
+            else:
+                M_nonint = spsla.svds(
+                    TM, k=1, return_singular_vectors=False
+                )[0]
+    
     fid = sum(abs(sp.array(coeff_list))**2)
-    msg = "Found a branch decomposition with with local bond dimensions {} and {} and coefficients {} and {}, with fidelity {}.".format(*dim_list, *coeff_list, fid)
+    msg = "Found a branch decomposition with with local (i1, i2) bond dimensions ({}, {}) and ({}, {}) and coefficients {} and {}.".format(dim_list_L[0], dim_list_R[0], dim_list_L[1], dim_list_R[1], *coeff_list)
     logging.info(msg)
-    if 1 - fid > pars["eps_fidelity"]:
-        msg = "Rejecting this decomposition."
+    msg = "Fidelity: {}".format(fid)
+    logging.info(msg)
+    if "M" in pars["system_for_records"]:
+        msg = "Non-interference on M: {}".format(M_nonint)
         logging.info(msg)
-        branch_list = [s]
+
+    if 1 - fid > pars["eps_fidelity"]:
+        msg = "Rejecting this decomposition due to too low fidelity."
+        logging.info(msg)
+        # s_orig instead of s, because s.update() may have caused a phase
+        # change.
+        branch_list = [s_orig]
+        coeff_list = [1.0]
+    elif "M" in pars["system_for_records"] and M_nonint > pars["eps_M_nonint"]:
+        msg = "Rejecting this decomposition due to too high interference on M."
+        logging.info(msg)
+        # s_orig instead of s, because s.update() may have caused a phase
+        # change.
+        branch_list = [s_orig]
         coeff_list = [1.0]
     else:
         msg = "Accepting this decomposition."
---
+++ find_branches_test.py
@@ -0,0 +1,158 @@
+import logging
+import itertools as itt
+import functools as fct
+import warnings
+from ncon import ncon
+from copy import deepcopy
+import scipy as sp
+import numpy as np
+import evoMPS
+import mps_ext
+import pickle
+import find_branches
+
+# Doesn't evoMPS have this function? Regardless, this doesn't really belong in
+# this module, but for testing purposes...
+def mps_overlap(M1, M2):
+    one = sp.array([1], dtype=M1.typ)
+    double_one = ncon((one, one), ([-1], [-2]))
+    l = double_one
+    for pos in range(1, M1.N+1):
+        A1 = M1.A[pos]
+        A2 = M2.A[pos]
+        l = ncon((l, A1, A2.conjugate()), ([1,2], [3,1,-1], [3,2,-2]))
+    assert(len(l) == 1)
+    return l[0,0]
+
+def mps_overlap_matrix(mpses_rows, mpses_columns=None, normalize=True):
+    # Given two lists of MPSes, returns a matrix of overlaps.
+    # Given one list of MPSes, returns the Grammian matrix.
+    # By default, vectors are normalized before overlaps are computed.
+    hermitian = mpses_columns is None
+    if hermitian:
+        mpses_columns = mpses_rows
+    overlaps = np.empty((len(mpses_rows), len(mpses_columns)),
+                        dtype=np.complex_)
+    for i in range(len(mpses_rows)):
+        for j in range(len(mpses_columns)):
+            if i <= j or not hermitian:
+                f = mps_overlap(mpses_rows[i], mpses_columns[j])
+            else:
+                f = np.conjugate(overlaps[j,i])
+            overlaps[i, j] = f
+    if normalize:
+        if hermitian:
+            row_norms = column_norms = np.diag(overlaps)
+        else:
+            row_norms = np.array([np.sqrt(mps_overlap(m, m))
+                                  for m in mpses_rows])
+            column_norms = np.array([np.sqrt(mps_overlap(m, m))
+                                     for m in mpses_columns])
+        # Divide elementwise. The newaxis trick makes the division be across
+        # rows, as opposed to columns.
+        overlaps /= column_norms
+        overlaps /= row_norms[:, np.newaxis]
+    return overlaps
+
+def main():
+    np.set_printoptions(linewidth=120, suppress=True)
+    warnings.filterwarnings("error")
+    seed = np.random.randint(1, 1e6)
+    logging.info("Seed: {}".format(seed))
+    np.random.seed(seed)
+
+    N = 100  # Number of sites
+    d = 2   # Physical dimension
+    q = [d]*(N+1)
+    noise_factor = 0e-4  # Coefficients for random noise to add to As
+
+    # Parameters for the block finding.
+    pars = {
+        "i1": None,
+        "i2": None,
+        "system_for_records": "RM",
+        "eps_fidelity": 1e-3,
+        "eps_M_nonint": 1e-3,
+        "k": 20,
+    }
+
+    # Bond dimensions of the terms in the sum
+    #Ds = [
+    #    [1,3,5,8,5,5,8,9,8,3,1],
+    #    [1,3,5,6,5,5,8,7,4,2,1]
+    #]
+    Ds = [
+        [3]*(N+1),
+        [3]*(N+1),
+        [3]*(N+1),
+        #[4,7,8,3,5,6,4,2,3,2,3,2,5,3,2],  # Requires N=14
+        #[5,4,8,3,6,9,7,4,5,8,6,5,7,4,5],
+        #[7,4,8,9,6,3,3,2,4,7,9,7,6,7,4],
+        #[1]*(N+1),
+        #[1]*(N+1),
+        #[25]*(N+1),
+        #[25]*(N+1),
+    ]
+    # Random generate coefficients for the sum. They square sum to 1.
+    orig_coeffs = np.random.rand(len(Ds)) + 1j*np.random.rand(len(Ds))
+    orig_coeffs /= np.linalg.norm(orig_coeffs)
+    orig_coeffs = list(orig_coeffs)
+
+    # Generate random MPSes of the given dimensions...
+    orig_terms = []
+    for b in range(len(Ds)):
+        term = evoMPS.mps_gen.EvoMPS_MPS_Generic(N, Ds[b], q)
+        term.randomize()
+        Ds[b] = term.D  # This may have changed due to updates by evoMPS
+        orig_terms.append(term)
+
+    # ...sort them by coefficient, for convenience...
+    order = np.argsort(-abs(np.array(orig_coeffs)))
+    orig_coeffs = list(map(orig_coeffs.__getitem__, order))
+    orig_terms = list(map(orig_terms.__getitem__, order))
+
+    assert(all(len(D) == N+1 for D in Ds))
+
+    # ...take their weighted sum and add some noise to it...
+    lincomb = mps_ext.add_MPS_list(orig_terms, coeffs=orig_coeffs)
+    lincomb.add_noise(noise_factor)
+
+    logging.info("Original coefficients: {}".format(orig_coeffs))
+    logging.info("Bond dimension of linear combination: \n{}"
+                 .format(lincomb.D))
+    logging.info("Bond dimensions of the original terms: \n{}"
+                 .format([t.D for t in orig_terms]))
+
+    # ...and decompose the linear combination back into terms with the same
+    # bond dimensions as the original ones.
+    branch_list, coeff_list = find_branches.find_branches_sparse(
+        lincomb, pars=pars
+    )
+
+    # Print possibly interesting things.
+    logging.info("Noise factor: {}".format(noise_factor))
+    logging.info("Overlap matrix of the original terms: \n{}"
+                 .format(abs(mps_overlap_matrix(orig_terms))))
+    logging.info("Overlap matrix of the found terms: \n{}"
+                 .format(abs(mps_overlap_matrix(branch_list))))
+    logging.info("Overlap matrix between found and original: \n{}"
+                 .format(abs(mps_overlap_matrix(branch_list, orig_terms))))
+    logging.info("Bond dimension of linear combination: \n{}"
+                 .format(lincomb.D))
+    logging.info("Bond dimensions of the original terms: \n{}"
+                 .format([t.D for t in orig_terms]))
+    logging.info("Bond dimensions of the found terms: \n{}"
+                 .format([t.D for t in branch_list]))
+    logging.info("Original coefficients: {}".format(orig_coeffs))
+    logging.info("abs thereof:           {}".format(abs(np.array(orig_coeffs))))
+    logging.info("Found coefficients:    {}".format(coeff_list))
+
+    # Sum up the found terms, and compute the fidelity with the original linear
+    # combination explicitly.
+    lincomb_reco = mps_ext.add_MPS_list(branch_list, coeffs=coeff_list)
+    fid_reco = mps_overlap(lincomb_reco, lincomb)
+    logging.info("Reconstructed fidelity: {} (abs {})"
+                 .format(fid_reco, abs(fid_reco)))
+
+if __name__ == "__main__":
+    main()
